a7316849e1539b97f257c75f5b8e3ffdbe37aefd
Masayuki1009 <smasayukii1009@gmail.com>
2022-12-16 15:51:39 +0900

    initial commit

---
+++ src/main/java/com/example/TsvHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsvHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsvHandlingApplication.class, args);
+	}
+
+}
