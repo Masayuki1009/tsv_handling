879807e47d156b6ba7d82b620cf81445de46f1d1
Masayuki1009 <smasayukii1009@gmail.com>
2022-12-16 16:11:54 +0900

    setup ok

--- src/main/java/com/example/TsvHandlingApplication.java
+++ src/main/java/com/example/TsvHandlingApplication.java
@@ -1,13 +1,57 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TsvHandlingApplication {
+	
+public static void main(String[] args) {
+		
+		// tsvファイルのpathを指定
+		String filePath = "/Users/shibatamasayuki/DeskTop/train.tsv";
+		List<String> tsvDataList = new ArrayList<>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			String line;
+			
+			
+			for(int i = 0; i <= 2; i++) {
+				line = br.readLine();
+				System.out.println("line : " + line);
+			}
+//			 br.readLine: 1行(改行などがされるまでの)
+//			while ((line = br.readLine()) != null) {
+//
+//				String[] data = line.split("\t", 0); // 行をタブで区切り、配列に変換
+//
+//				for (String eachData : data) {
+//					System.out.println("eachData情報 : " + eachData);
+//					tsvDataList.add(eachData);
+//					System.out.println("tsvDataList : " + tsvDataList);
+//					System.out.println("lineのlength : " + line.length());
+//				}
+//			}
+			
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 
-	public static void main(String[] args) {
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
 		SpringApplication.run(TsvHandlingApplication.class, args);
 	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(TsvHandlingApplication.class, args);
+//	}
 
 }
